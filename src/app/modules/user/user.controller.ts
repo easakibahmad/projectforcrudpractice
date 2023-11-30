92d1c0dfa1c18d828ca538cae32dbec29d90a6bb
easakibahmad <sakib.sgc.du@gmail.com>
2023-12-01 04:19:52 +0600

    higher order function created to resolve try-catch repetition

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,32 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import httpStatus from "http-status";
 
 import { NextFunction, Request, Response } from "express";
 import { UserServices } from "./user.service";
-import sendResponse from '../../utils/sendResponse';
+import sendResponse from "../../utils/sendResponse";
+import { RequestHandler } from "express-serve-static-core";
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { password, student: studentData } = req.body;
-
-    const result = await UserServices.createStudentIntoDB(
-      password,
-      studentData
-    );
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Student is created successfully",
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
+//create higher order function
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
 };
+
+const createStudent = catchAsync(async (req, res, next) => {
+  const { password, student: studentData } = req.body;
+
+  const result = await UserServices.createStudentIntoDB(password, studentData);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Student is created successfully",
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
